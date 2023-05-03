f599db769d9e716919e536cd9135abdeeff3818a
Jay Dickson <21349032+jaydickson02@users.noreply.github.com>
2023-05-03 19:28:17 +1000

    Reduced Runtime

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # Run the script
 while true
 do
-    python3 main.py --train --batch_size=10 --episodes=200 --runtime=1500
+    python3 main.py --train --batch_size=10 --episodes=200 --runtime=500
 done
 
 
